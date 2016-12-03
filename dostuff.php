00f6ece828bbb7599611d2b8bfd8bf0bf207c510
michzzzm <michzzzm@gmail.com>
2016-12-02 19:54:49 -0600

    Update dostuff.php

--- dostuff.php
+++ dostuff.php
@@ -5,28 +5,36 @@ session_start();
 
 <?php
 
-// setKey ('1Y_4Zi1yQC3yFSqaI9ODL_Cg5rLAuGEhp69l-BC2bHg8');
+$_SESSION ['invasiveskey'] = makeKey ('https://docs.google.com/spreadsheets/d/1Y_4Zi1yQC3yFSqaI9ODL_Cg5rLAuGEhp69l-BC2bHg8/edit#gid=0');
+$_SESSION ['microbekey'] = makeKey ('https://docs.google.com/spreadsheets/d/1yEIn5lkZv-plluXnTBdOWKeH4S2kzX5DxCqIA7cnWgc/edit#gid=0');
 
-if (isset ($_POST ['invasives'])){
-//	echo 'done';
-	setKey ( 'https://docs.google.com/spreadsheets/d/1Y_4Zi1yQC3yFSqaI9ODL_Cg5rLAuGEhp69l-BC2bHg8/edit#gid=0');
-	dostuff();
+
+if (isset ($_POST ['invasives']) ){
+	if ( !isset ($_SESSION ['key']) || $_SESSION['key'] != $_SESSION['invasiveskey'] ){
+		setKey ($_SESSION['invasiveskey']);
+		dostuff();
+	}
+	else if ( $_SESSION['key'] == $_SESSION['invasiveskey'] ){
+		unsetKey ();
+	}
 }
+
 else if (isset ($_POST ['anat'])){
 	
 }
 else if (isset ($_POST ['microbe'])){
-	setKey ('https://docs.google.com/spreadsheets/d/1yEIn5lkZv-plluXnTBdOWKeH4S2kzX5DxCqIA7cnWgc/edit#gid=0');
-	dostuff();
+	if ( !isset ($_SESSION ['key']) || $_SESSION['key'] != $_SESSION['microbekey'] ){
+		setKey ($_SESSION['microbekey']);
+		dostuff();
+	}
+	else if ( $_SESSION['key'] == $_SESSION['microbekey'] ){
+		unsetKey ();
+	}
 }
 else if (isset ($_POST ['rocks'])){
 }
 else if (isset ($_POST ['diy'])){
 }
-
-
-
-
 if (!isset ($_SESSION ['count'])){
 	if (isset ($_SESSION ['key'])){
 		$_SESSION ['count'] = 1; 
@@ -34,7 +42,6 @@ if (!isset ($_SESSION ['count'])){
 	}
 //	echo "complete refresh";
 }
-
 else {
 	if (isset ($_POST ['submit'])  ){
 		if (empty ($_POST ['textbox'])){
@@ -54,12 +61,10 @@ else {
 //			echo "incorrect";
 		}
 	}
-
 	if (isset ($_POST ['skip'])){
 		dostuff();
 		$_SESSION ['streak'] = 0;
 	}
-
 	if (isset ($_POST['freeans']) ) {
 		$_SESSION ['show'] = true;
 		// $_SESSION ['names'][0] . " / " . $_SESSION ['names'][1] ;
@@ -70,33 +75,24 @@ else {
 		$_SESSION ['show'] = false;
 	//	unset ($_SESSION['check']);  //???/??
 	}
-	/*if (isset ($_POST ['website'])){
-		echo '<script type="text/javascript" language="javascript">    // fixed to onclick -> no popups 
-		window.open("' . $_SESSION['context'] .'" ); 
-		</script>'; 
-	}  */
-}
 
+}
 function sheets ($key){
 	$url = 'https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec?id='
 		.  $key. '&sheet=Sheet1';
-
 	$result = file_get_contents($url);
 	$arr = json_decode($result, true);
 	return $arr;
 }
-
 function dostuff (){
 	$_SESSION ['names'] = name ($_SESSION ['arr']);
 //	var_dump ( $_SESSION ['names']);
 //	echo $_SESSION ['names'][0] . " / " . $_SESSION ['names'][1] ;
-
 	$urls = img ($_SESSION ['names'][0], 20);
 	$_SESSION ['img'] = $urls [0];
 	$_SESSION ['context'] = $urls [1];
 //	echo $_SESSION ['img'];
 //	echo $_SESSION ['context'];
-
 	$_SESSION ['show'] = false;
 	if (!isset ($_SESSION ['count']))
 		$_SESSION ['count'] = 1;
@@ -105,18 +101,25 @@ function dostuff (){
 	unset ($_SESSION ['check']);
 }
 
-function setKey($url){	
-	$key = substr ($url , (strrpos ($url, "/d/")+3) , (strrpos ($url, "edit")-1) - (strrpos ($url, "/d/")+3) );
+function makeKey ($url){
+	return substr ($url , (strrpos ($url, "/d/")+3) , (strrpos ($url, "edit")-1) - (strrpos ($url, "/d/")+3) ) ; 
+}
+
+function setKey($key){	
+	// $key = substr ($url , (strrpos ($url, "/d/")+3) , (strrpos ($url, "edit")-1) - (strrpos ($url, "/d/")+3) );
 //	echo $key; 
 	$_SESSION ['key'] = $key;
 	$_SESSION ['arr'] = sheets ($_SESSION ['key']);
 	unset ($_SESSION ['count']);
 }
 
+function unsetKey(){	
+
+	unset ($_SESSION ['key']);
+}
 function name ($arr){
 	$index = rand (0,count ($arr ['Sheet1'])-1);
 	$names = array ();
-
 	$name = $arr ['Sheet1'] [$index]['hard'];
 	if (strpos ($name , ',') == FALSE)
 		$names [] = $name;
@@ -137,16 +140,13 @@ function name ($arr){
 	array_walk($names, 'trim_value');
 	return $names; 
 }
-
 function trim_value(&$value) 
 { 
     $value = trim($value); 
 }
-
 function getName (){
 	return $_SESSION ['names'][0] . " / " . $_SESSION['names'][1];
 }
-
 function img ($name , $num){
 	$url = 'http://api.ababeen.com/api/images.php?q='. str_replace (' ', '+', $name) .'&count=' . $num;
 //	echo $url;
@@ -157,10 +157,7 @@ function img ($name , $num){
 	curl_close($ch);
 	
 	$result = json_decode($result, true);
-
 	$index = rand (1, $num); 
 	return array ($result [$index] ["url"],  $result [$index] ["originalContextUrl"]);
 }
-
-
 ?>
